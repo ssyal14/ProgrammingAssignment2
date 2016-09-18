837289bd3687ea66988d3fb645fa1dee45d7c5c1
ssyal14 <shivanisyal82@yahoo.com>
2016-09-18 15:18:25 +0530

    Programming assignment 2
    Programming assignment of matrix inversion and cache

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,57 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y  
+    m <<- NULL 
+  }
+  get <- function() x 
+  setinverse <- function(solve) 
+    m<<- solve
+  getinverse <- function() m 
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()                
+  if(!is.null(m)){                   
+    message("Data from cache")   
+    return(m)                           
+  }
+  data <- x$get()                    
+  m <- solve(data, ...)               
+  x$setinverse(m)    
+  return(m)        
+   ## Return a matrix that is the inverse of 'x'
 }
+
+
+# Example using the program :
+#
+# x=matrix(c(2, 4, 3, 1), nrow=2,ncol=2)     
+# m = makeCacheMatrix(x)
+# m$get()
+#       [,1] [,2]
+# [1,]    2    3
+# [2,]    4    1
+#
+# Original computation in first time
+# cacheSolve(m)
+#       [,1] [,2]
+#  [1,] -0.1  0.3
+#  [2,]  0.4 -0.2
+#
+# Now the data will be from cache
+# cacheSolve(m)
+# Data from cache
+#       [,1] [,2]
+#  [1,] -0.1  0.3
+#  [2,]  0.4 -0.2
+
+
